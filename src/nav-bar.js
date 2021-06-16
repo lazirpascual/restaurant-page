0ad1845626fcf3459a9b87bde90ea6cfaecf88b3
Lazir Pascual <lazirpascual@gmail.com>
2021-06-16 01:58:42 -0400

    change nav bar spacing

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -5,14 +5,17 @@ export const createNavBar = () => {
 
     // create three sections; home, menu, and about
     const home = document.createElement('div');
+    home.classList.add('nav-items');
     home.textContent = "HOME";
     home.id = 'home-id';
     navBar.appendChild(home);
     const menu = document.createElement('div');
+    menu.classList.add('nav-items');
     menu.textContent = "MENU";
     menu.id = 'menu-id';
     navBar.appendChild(menu);
     const about = document.createElement('div');
+    about.classList.add('nav-items');
     about.textContent = "ABOUT";
     about.id = 'about-id';
     navBar.appendChild(about);
