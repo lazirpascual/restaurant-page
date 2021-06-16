b17c420085c76c13f3e31ff807f5b76e49f04dbf
Lazir Pascual <lazirpascual@gmail.com>
2021-06-16 01:32:43 -0400

    add content to about page

--- src/about.js
+++ src/about.js
@@ -8,6 +8,37 @@ export const createAboutPage = () => {
     htmlContainer.classList.remove('menu-background');
     htmlContainer.classList.add('about-background');
 
+    // create section for restaurant info
+    const mainContainer = document.querySelector('#content');
+    const restaurantInfo = document.createElement('div');
+    restaurantInfo.classList.add('restaurant-info');
+
+    //create three sections in restaurant info, location, hours, and contact
+    const location = document.createElement('div');
+    location.textContent = "Location";
+    const street = document.createElement('div');
+    street.classList.add('info-box');
+    street.textContent = "82 King Street, Kitchener, ON";
+    location.appendChild(street);
+    restaurantInfo.appendChild(location);
+
+    const hours = document.createElement('div');
+    hours.textContent = "Hours";
+    const availability = document.createElement('div');
+    availability.classList.add('info-box');
+    availability.textContent = "Mon - Sat, 10am - 9pm";
+    hours.appendChild(availability);
+    restaurantInfo.appendChild(hours);
+
+    const contact = document.createElement('div');
+    contact.textContent = "Contact";
+    const email = document.createElement('div');
+    email.classList.add('info-box');
+    email.textContent = "dyneforfree@hotmail.ca";
+    contact.appendChild(email);
+    restaurantInfo.appendChild(contact);
+
     createNavBar();
+    mainContainer.appendChild(restaurantInfo);
     addEventListeners();
 }
